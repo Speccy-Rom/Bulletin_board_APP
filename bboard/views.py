5cf5d20241a885b5ac7ff19a1cf9831cd0234c6b
Speccy-rom <speccy.rom@gmail.com>
2021-01-08 00:47:26 +0300

    add template,редактор BbAdmin

--- bboard/views.py
+++ bboard/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+from .models import Bb
 
 
 def index(request):
-    return HttpResponse('Здесь будет выведен список объявлений.')
+    bbs = Bb.objects.all()
+    return render(request, 'index.html', {'bbs': bbs})
 
