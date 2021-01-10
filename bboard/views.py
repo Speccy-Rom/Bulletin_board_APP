b00fe7bc71660270c247411e1de1f8fb265a82f2
Speccy-rom <speccy.rom@gmail.com>
2021-01-10 23:44:15 +0300

    add forms (CreateView)

--- bboard/views.py
+++ bboard/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 from .models import Bb, Rubric
+from .forms import BbForm
 
 
 def index(request):
@@ -20,3 +22,14 @@ def by_rubric(request, rubric_id):
         'current_rubric': current_rubric,
     }
     return render(request, 'by_rubric.html', context)
+
+
+class BbCreateView(CreateView):
+    template_name = 'create.html'
+    form_class = BbForm
+    success_url = reverse_lazy('index')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['rubrics'] = Rubric.objects.all()
+        return context
